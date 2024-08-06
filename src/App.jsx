bd391d5668d6029b78463c24dc5058c02a1b3fb0
윤아현 <74540582+Yoon-Ahyeon@users.noreply.github.com>
2024-08-06 15:28:49 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+// import styled from 'styled-components';
+import Service from "../src/Service";  
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Service />  
+  );
+}
+
+export default App;
